63dfd8678cf5f033c971e643dc56afca4a550791
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-31 21:30:45 +0600

    changes

--- library_management/settings.py
+++ library_management/settings.py
@@ -166,9 +166,9 @@ LOGIN_URL = '/accounts/login/'
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
