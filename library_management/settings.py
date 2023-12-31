3fa44a9774a98df909238f72da1e974d6a519414
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-31 21:25:54 +0600

    changes

--- library_management/settings.py
+++ library_management/settings.py
@@ -37,6 +37,8 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    'https://library-9u9q.onrender.com/', 'https://*.127.0.0.1']
 
 
 # Application definition
