e6a271649ed3db6b24064af9028a513642df725f
Sanjit Sarkar <sanjit.est14@gmail.com>
2024-01-01 14:10:56 +0600

    updated

--- library_management/settings.py
+++ library_management/settings.py
@@ -183,5 +183,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("EMAIL")
+EMAIL_HOST_USER = os.environ.get('EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
